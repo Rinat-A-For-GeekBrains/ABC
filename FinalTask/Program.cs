68306c8731df3a8bf73b0fbe1294540ffb2e07d1
unknown <alimbekovrl@gmail.com>
2023-03-26 14:31:12 +0500

    сделал ввод массива заданной длины

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -7,3 +7,16 @@
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
 
+
+Console.Write("Введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+string [] stringArray = new string [m];
+
+void array(string [] stringArray)
+{
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+     Console.WriteLine($"Введите {i+1} элемент массива");
+     stringArray[i] = Console.ReadLine();
+  }
+}
